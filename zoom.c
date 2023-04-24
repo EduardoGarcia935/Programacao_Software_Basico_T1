f16f0d2da3998221b568642ab70e097a1610120c
EduardoGarcia935 <eduardo.agarcia117@gmail.com>
2023-04-24 20:07:43 -0300

    feat: algoritmo pronto

--- zoom.c
+++ zoom.c
@@ -1,93 +1,239 @@
 #include <stdio.h>
 #include "lib_ppm.h"
 
+// All black = 1
+// Middle the same, rest black = 2
+// Middle black, rest the same = 3
+// all respective color = 4
+
+int check_padrao(int cor)
+{
+	if (cor <= 74)
+		return 1;
+	if (cor <= 134)
+		return 2;
+	if (cor <= 179)
+		return 3;
+	return 4;
+}
+
 int main()
 {
 	struct image_s data;
 	struct image_s *image = &data;
+	struct image_s data_new_image;
+	struct image_s *new_image = &data_new_image;
 	int i, j, r;
 
-	/*
-		Moral disso aqui,
-		o método read_ppm retorna 0 caso a leitura da foto tinha sido feita com sucesso,
-		caso contrário ele retorna -1.
-		Por isso ele faz aquele if (r == 0) logo abaixo
-	*/ 
 	r = read_ppm("lena.ppm", image);
+	int is_new_ok = new_ppm(new_image, image->width * 3, image->height * 3);
 
-	/*
-		tipo, todos os dados da imagem lida ficarão nesse "Objeto" / Struct image,
-		ai temos:
-
-		image->width = valor inteiro
-		image->height = valor inteiro
-
-		image->pix[POS].r = retorna o valor R do código RGB do pixel na posição "POS"
-		image->pix[POS].g = retorna o valor G do código RGB do pixel na posição "POS"
-		image->pix[POS].b = retorna o valor B do código RGB do pixel na posição "POS"
-
-		Sor cantou a pedra para ler e percorrer todos os pixeis da imagem,
-
-		assumindo que essa linha esteja dentro de dois "for()" para ler a matriz, sendo 'j' do 'for' interno e 'i' do for mais externo
-		image->[j * image->width + i].r
-
-		outro exemplo, sabemos q a imagem do sor tem 512px por 512px,
-		se quisermos acessar o código rgb do exato último pixel, temos:
-
-		printf("RGB (%hu , %hu , %hu)\n",image->pix[511 *image->width + 511].r , image->pix[511 * image->width +511].g , image->pix[511 * image->width +511].b);
-
-		OBS: o jeito de 'printar' esses valores RGB no console é usando '%hu', pq não são inteiros, são uma espécie de 'char'
-
-	*/
-
-
-	if (r == 0)
+	if (r == 0 && is_new_ok == 0)
 	{
 		printf("width: %d, height: %d\n", image->width, image->height);
-
 		for (j = 0; j < image->height; j++)
 		{
+			// Para j 0
+			//
+			// Criar j0, j0+1, j0+2
 			for (i = 0; i < image->width; i++)
 			{
-				// Código do sor pra printar o código hexadecimal da cor de cada pixel
-				// printf("(%d %d) %02x %02x %02x\n", i, j,
-				// 	   image->pix[j * image->width + i].r,
-				// 	   image->pix[j * image->width + i].g,
-				// 	   image->pix[j * image->width + i].b);
+				// All black = 1
+				// Middle the same, rest black = 2
+				// Middle black, rest the same = 3
+				// all respective color = 4
 
-				printf("Pixel (%d , %d) - RGB(%hu , %hu , %hu)\n", i ,j,
-						image->pix[j * image->width + i].r,
-						image->pix[j * image->width + i].g,
-						image->pix[j * image->width + i].b);
+				int padrao_r = check_padrao(image->pix[j * image->width + i].r);
+				int padrao_g = check_padrao(image->pix[j * image->width + i].g);
+				int padrao_b = check_padrao(image->pix[j * image->width + i].b);
+				if (padrao_r == 1)
+				{
+					// All black = 1
+					// Middle the same, rest black = 2
+					// Middle black, rest the same = 3
+					// all respective color = 4
+
+					// PRIMEIRA COLUNA
+					new_image->pix[j * 3 * new_image->width + (3 * i)].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].b = 0;
+				}
+				else if (padrao_r == 2)
+				{
+					// PRIMEIRA COLUNA
+					new_image->pix[j * 3 * new_image->width + (3 * i)].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].r = 255;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].b = 0;
+				}
+				else if (padrao_r == 3)
+				{
+					// PRIMEIRA COLUNA
+					new_image->pix[j * 3 * new_image->width + (3 * i)].r = 255;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].r = 255;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].b = 0;
+				}
+				else if (padrao_r == 4)
+				{
+					// PRIMEIRA COLUNA
+					new_image->pix[j * 3 * new_image->width + (3 * i)].r = 255;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].r = 255;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i)].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].r = 255;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i)].b = 0;
+				}
+				if (padrao_g == 1)
+				{
+					// SEGUNDA COLUNA
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].b = 0;
+				}
+				else if (padrao_g == 2)
+				{
+					// SEGUNDA COLUNA
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].g = 255;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].b = 0;
+				}
+				else if (padrao_g == 3)
+				{
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].g = 255;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].g = 255;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].b = 0;
+				}
+				else if (padrao_g == 4)
+				{
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].g = 255;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].g = 255;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 1].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].g = 255;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 1].b = 0;
+				}
+				if (padrao_b == 1)
+				{
+					// TERCEIRA COLUNA
+
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].b = 0;
+				}
+				else if (padrao_b == 2)
+				{
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].b = 0;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].b = 255;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].b = 0;
+				}
+				else if (padrao_b == 3)
+				{
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].b = 255;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].b = 0;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].b = 255;
+				}
+				else if (padrao_b == 4)
+				{
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[j * 3 * new_image->width + (3 * i) + 2].b = 255;
+
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 1) * new_image->width + (3 * i) + 2].b = 255;
+
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].r = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].g = 0;
+					new_image->pix[(j * 3 + 2) * new_image->width + (3 * i) + 2].b = 255;
+				}
 			}
 		}
-		// No fucking ideia do q isso faz
-		image->pix[50 * image->width + 20].r = 255 ;
-		image->pix[50 * image->width + 20].g = 255 ;
-		image->pix[50 * image->width + 20].b = 255 ;
-
-
-		
-		write_ppm("lena_copy.ppm", image);
 
 		free_ppm(image);
-	}
 
-	// Exemplo da criação de uma nova imagem ppm.
-	// Totalmente preta com apenas alguns pontos coloridos
-	r = new_ppm(image, 675, 428);
-
-	if (r == 0)
-	{
-		image->pix[100 * image->width + 125].r = 255;
-		image->pix[27 * image->width + 440].g = 255;
-
-		image->pix[371 * image->width + 10].r = 192;
-		image->pix[371 * image->width + 10].g = 192;
-		image->pix[371 * image->width + 10].b = 192;
-
-		write_ppm("test.ppm", image);
-		free_ppm(image);
+		write_ppm("nicolodi.ppm", new_image);
+		free_ppm(new_image);
 	}
 
 	return 0;
